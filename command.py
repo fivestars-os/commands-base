9e73199dd579b93bc8757482e0894e1f7e1262b2
Josh Wilson <josh.wilson@fivestars.com>
2020-04-03 10:48:45 -0700

    Update to python:3.8-slim
    This also adds the `--log-level` argument for all commands and provides a basic logging config
    for any user-defined commands. This means commands can now create `logger` instances and use them
    instead of `print()`.

--- command.py
+++ command.py
@@ -1,24 +1,37 @@
 import argparse
+import logging
 import os
 import pkgutil
 
-if __name__ == '__main__':
 
-    COMMANDS_DIRECTORY = 'commands'
+def main(project_name):
+    # Discover all the commands defined in derived image
+    parser = argparse.ArgumentParser(description=f"Managing {project_name} commands")
+    parser.add_argument("--log-level", choices=logging._nameToLevel, default="INFO")
 
-    parser = argparse.ArgumentParser(description='Managing %s commands' % os.environ.get("PROJECT_NAME"))
-    subparsers = parser.add_subparsers(help='sub commands')
+    subparsers = parser.add_subparsers(help="sub commands")
+    gather_commands(subparsers, "commands")
 
-    subcommands = []
-    for _, command_name, _ in pkgutil.iter_modules([COMMANDS_DIRECTORY]):
-        path = "%s.%s" % (COMMANDS_DIRECTORY, command_name)
-        commands_import = __import__(path)
-        subcommands.append(getattr(commands_import, command_name))
-
-    for subcommand in subcommands:
-        subcommand.parse_args(subparsers)
-
+    # Actually parse the arguments
     args = parser.parse_args()
 
+    # Apply the log level to the basic config
+    logging.basicConfig(
+        format="%(levelname)-8s %(name)s(%(lineno)d) %(message)s",
+        level=logging._nameToLevel[args.log_level],
+    )
+
+    # Invoke the requested command
     args.func(args)
 
+
+def gather_commands(subparsers, directory):
+    # Search for available commands and gather their arguments
+    for _, command_name, _ in pkgutil.iter_modules([directory]):
+        commands_package = __import__(f"{directory}.{command_name}")
+        command_module = getattr(commands_package, command_name)
+        command_module.parse_args(subparsers)
+
+
+if __name__ == "__main__":
+    main(os.environ.get("PROJECT_NAME"))
