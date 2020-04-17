fbb0b1e4a2a61bfb952f32dad795289e079ccc70
Josh Wilson <josh.wilson@fivestars.com>
2020-04-16 23:43:14 -0700

    [BREAKING CHANGES] Move the commands.py script to /usr/local/bin
    The `aladdin` `cmd` command needs to be updated to find the `command.py` script in it's new
    location and name.
    
    `k.kub_exec(pod_name, "%s-commands" % app_name, "/usr/local/bin/aladdin_cmd", *command_args)`
    
    This change is necessary to support `--with-mount` situations, since if one were to mount their
    `commands` project directory at `/code` it would stomp on the currently baked-in `commands.py`,
    since it's placed in `/code`.
    
    This also adds the poetry package manager to the base image and some useful `logging` utils to allow
    clients to write out more informational logs.

--- command.py
+++ command.py
@@ -1,37 +1,61 @@
+#!/usr/bin/env python3
+
 import argparse
 import logging
-import os
+import pathlib
 import pkgutil
+import os
+
+import coloredlogs
+import verboselogs
 
 
-def main(project_name):
+def main(project_name: str):
     # Discover all the commands defined in derived image
     parser = argparse.ArgumentParser(description=f"Managing {project_name} commands")
-    parser.add_argument("--log-level", choices=logging._nameToLevel, default="INFO")
+    parser.add_argument(
+        "--log-level",
+        choices=sorted(logging._nameToLevel, key=lambda name: logging._nameToLevel[name]),
+        default="INFO",
+    )
 
-    subparsers = parser.add_subparsers(help="sub commands")
-    gather_commands(subparsers, "commands")
+    # Import our commands and ask them to populate the subparsers with their argument configurations
+    subparsers = parser.add_subparsers(help="Sub-commands")
+    commands_directory = pathlib.Path("/code") / "commands"
+    gather_commands(subparsers, commands_directory)
 
     # Actually parse the arguments
     args = parser.parse_args()
 
-    # Apply the log level to the basic config
-    logging.basicConfig(
-        format="%(levelname)-8s %(name)s(%(lineno)d) %(message)s",
+    # Use the provided log level and upgrade our logging capabilities
+    coloredlogs.install(
         level=logging._nameToLevel[args.log_level],
+        fmt="%(levelname)-8s %(name)s(%(lineno)d) %(message)s",
+        level_styles=dict(
+            spam=dict(color="white", faint=True),
+            debug=dict(color="black", bold=True),
+            verbose=dict(color="blue"),
+            info=dict(color="cyan"),
+            notice=dict(color="magenta"),
+            warning=dict(color="yellow"),
+            success=dict(color="green", bold=True),
+            error=dict(color="red"),
+            critical=dict(color="red", bold=True),
+        ),
     )
 
     # Invoke the requested command
     args.func(args)
 
 
-def gather_commands(subparsers, directory):
-    # Search for available commands and gather their arguments
+def gather_commands(subparsers: argparse.Action, directory: pathlib.Path):
+    """Search for available commands and gather their arguments."""
     for _, command_name, _ in pkgutil.iter_modules([directory]):
-        commands_package = __import__(f"{directory}.{command_name}")
+        commands_package = __import__(f"{directory.stem}.{command_name}")
         command_module = getattr(commands_package, command_name)
         command_module.parse_args(subparsers)
 
 
 if __name__ == "__main__":
+    logging.setLoggerClass(verboselogs.VerboseLogger)
     main(os.environ.get("PROJECT_NAME"))
